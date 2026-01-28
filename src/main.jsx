6b390fff3aeb8621e3ed8a64797c6c991dc2f86b
Foxiary <042tranvuthuytrang@gmail.com>
2026-01-28 22:19:13 +0700

    Enhance mobile responsiveness across the application with CSS updates and new responsive utilities

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import store from "./store"
 import { Provider } from "react-redux";
+import "./index.css";
+import "./responsive.css";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
